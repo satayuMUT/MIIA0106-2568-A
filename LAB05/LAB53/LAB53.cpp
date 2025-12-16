1a7f5122e9ba1093325a73fe5d4f7054d279e302
SATAYU THUPMAENG <6711110185@mut.ac.th>
2025-12-16 16:36:27 +0700

    การทดลองที่ 5

---
+++ LAB05/LAB53/LAB53.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    int x;
+
+    do {
+        cout << "enter of x" << endl;
+        cin >> x;
+    } while (x != 0);
+
+    return 0;
+}
