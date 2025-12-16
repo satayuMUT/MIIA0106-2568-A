b5825b9a54540c2f1a470015b607f47ee3a547cf
SATAYU THUPMAENG <6711110185@mut.ac.th>
2025-12-16 16:27:07 +0700

    การทดลองที่ 5

---
+++ LAB05/LAB501/LAB501.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+double calculateArea(double radius) {
+	return 3.14159 * radius * radius;
+}	
+double calculateArea(double length, double width) {
+	return length * width;
+}
+double calculateArea(double base, double height, bool isTriangle) {
+	return 0.5 * base * height;
+}
+int main() {
+	double radius, length, width, base, height;
+
+	cout << "Enter radius of circle: ";
+	cin >> radius;
+	cout << "Area of circle: " << calculateArea(radius) << endl;
+
+	cout << "Enter length and width of rectangle: ";
+	cin >> length >> width;
+	cout << "Area of rectangle: " << calculateArea(length, width) << endl;
+
+	cout << "Enter base and height of triangle: ";
+	cin >> base >> height;
+	cout << "Area of triangle: " << calculateArea(base, height, true) << endl;
+
+	return 0;
+}
---
+++ LAB05/LAB503/LAB503.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	int x;
+
+	do {
+		cout << "enter of x" << endl;
+		cin >> x;
+	} while (x != 0);
+	return 0;
+}
